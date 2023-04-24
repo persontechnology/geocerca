08e5e3318c94b760c624500ce5befae151b1e611
persontechnology <persontechnologys@gmail.com>
2023-04-24 14:05:28 -0500

    agrd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -111,12 +111,12 @@ window.initMap = initMap;
 </script>
 
 
-
+<script src="https://unpkg.com/@googlemaps/markerclusterer/dist/index.min.js"></script>
   <script
       src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBDxUyVFlNpM-HwzkAokj9g1I1OOpS4kZI&callback=initMap&v=weekly"
       defer
   ></script>
-  <script src="https://unpkg.com/@googlemaps/markerclusterer/dist/index.min.js"></script>
+  
   @endpush
 
 
