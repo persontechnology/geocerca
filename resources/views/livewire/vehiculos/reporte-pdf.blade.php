198bdb5c649f1111e06687e27f09b4032f3790d4
persontechnology <persontechnologys@gmail.com>
2024-07-05 11:24:42 -0500

    add 100 filtors

--- resources/views/livewire/vehiculos/reporte-pdf.blade.php
+++ resources/views/livewire/vehiculos/reporte-pdf.blade.php
@@ -31,6 +31,7 @@
                             <option value="10">10</option>
                             <option value="50">50</option>
                             <option value="100">100</option>
+                            <option value="1000">1000</option>
                         </select>
                     </div>
                 </div>
