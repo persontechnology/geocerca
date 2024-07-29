6ebc28628c11aad950c8167448ec98a8e2af3976
persontechnology <persontechnologys@gmail.com>
2024-07-29 15:23:33 -0500

    envios

--- config/datatables-html.php
+++ config/datatables-html.php
@@ -11,7 +11,7 @@ return [
      * Default table attributes when generating the table.
      */
     'table' => [
-        'class' => 'table',
+        'class' => 'table table-striped table-hover table-bordered',
         'id'    => 'dataTableBuilder',
     ],
 
--- resources/views/movilizacion/action.blade.php
+++ resources/views/movilizacion/action.blade.php
@@ -1,20 +1,22 @@
 
-<input type="checkbox" name="omids[]" class="item-om-table" value="{{ $om->id }}">
+<div class="d-inline-flex">
+    <input type="checkbox" name="omids[]" class="item-om-table mr-2" value="{{ $om->id }}">
 
-<div class="list-icons">
-    <div class="dropdown">
-        <a href="#" class="list-icons-item" data-toggle="dropdown">
-            <i class="icon-menu9"></i>
-        </a>
+    <div class="list-icons">
+        <div class="dropdown">
+            <a href="#" class="list-icons-item" data-toggle="dropdown">
+                <i class="icon-menu9"></i>
+            </a>
 
-        <div class="dropdown-menu dropdown-menu-left">
-            <a href="{{ route('odernMovilizacionPdf',$om->id) }}" target="_blank" class="dropdown-item"><i class="fa-solid fa-file-pdf"></i> PDF</a>
-            <a href="{{ route('odernMovilizacionLecturas',$om->id) }}" class="dropdown-item"><i class="fa-solid fa-file-pdf text-warning"></i> Lecturas</a>
-            <div class="dropdown-divider"></div>
-            <a href="{{ route('odernMovilizacionEditar',$om->id) }}" class="dropdown-item"><i class="fa-solid fa-pen-to-square text-primary"></i> Editar</a>
-            <a href="#" onclick="event.preventDefault();eliminar(this);" data-id="{{ $om->id }}" data-url="{{ route('odernMovilizacionEliminar') }}" data-msg="Está seguro de eliminar Orden de movilización {{ $om->numero }}!" class="dropdown-item"><i class="fa-solid fa-trash text-danger"></i> Eliminar</a>
-            
+            <div class="dropdown-menu dropdown-menu-left">
+                <a href="{{ route('odernMovilizacionPdf',$om->id) }}" target="_blank" class="dropdown-item"><i class="fa-solid fa-file-pdf"></i> PDF</a>
+                <a href="{{ route('odernMovilizacionLecturas',$om->id) }}" class="dropdown-item"><i class="fa-solid fa-file-pdf text-warning"></i> Lecturas</a>
+                <div class="dropdown-divider"></div>
+                <a href="{{ route('odernMovilizacionEditar',$om->id) }}" class="dropdown-item"><i class="fa-solid fa-pen-to-square text-primary"></i> Editar</a>
+                <a href="#" onclick="event.preventDefault();eliminar(this);" data-id="{{ $om->id }}" data-url="{{ route('odernMovilizacionEliminar') }}" data-msg="Está seguro de eliminar Orden de movilización {{ $om->numero }}!" class="dropdown-item"><i class="fa-solid fa-trash text-danger"></i> Eliminar</a>
+                
+            </div>
         </div>
+        
     </div>
-    
 </div>
